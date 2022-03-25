843cba286c6d8dcbe9fe292726f580379b4430f6
samdai01 <daisam1215@gmail.com>
2022-03-24 18:38:20 -0700

    created insert and view commands for database

--- database_queries.py
+++ database_queries.py
@@ -26,3 +26,49 @@ def create_tables():
     cursor.execute("CREATE TABLE IF NOT EXISTS on_hold (id INT AUTO_INCREMENT PRIMARY KEY, title TEXT, author TEXT, year INT, isbn INT, name TEXT)")
     connection.commit()
     connection.close()
+    
+   
+"""
+Inserts a new book to the specified table. Enter information in the entry text boxes.
+"""
+def insert_book(title, author, year, isbn, name, table):
+    connection = mysql.connector.connect(host = 'localhost', user = 'root', password = 'test', database = "library_data")
+    cursor = connection.cursor()
+    
+    if table == 0:
+        sql = "INSERT INTO books (title, author, year, isbn, name) VALUES (%s, %s, %s, %s, %s)"
+        val = (title, author, year, isbn, name)
+        cursor.execute(sql, val)
+    elif table == 1:
+        sql = "INSERT INTO checked_out (title, author, year, isbn, name) VALUES (%s, %s, %s, %s, %s)"
+        val = (title, author, year, isbn, name)
+        cursor.execute(sql, val)
+    elif table == 2:
+        print("Test")
+        sql = "INSERT INTO on_hold (title, author, year, isbn, name) VALUES (%s, %s, %s, %s, %s)"
+        val = (title, author, year, isbn, name)
+        cursor.execute(sql, val)
+    connection.commit()
+    connection.close()
+
+"""
+Views all books in catalog.
+"""
+def view_all():
+    connection = mysql.connector.connect(host = 'localhost', user = 'root', password = 'test', database = "library_data")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM books ORDER BY title")
+    books = cursor.fetchall()
+    connection.close()
+    return books
+
+"""
+Views all books that are checked out.
+"""
+def view_checkedOut():
+    connection = mysql.connector.connect(host = 'localhost', user = 'root', password = 'test', database = "library_data")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM checked_out ORDER BY title")
+    books = cursor.fetchall()
+    connection.close()
+    return books
